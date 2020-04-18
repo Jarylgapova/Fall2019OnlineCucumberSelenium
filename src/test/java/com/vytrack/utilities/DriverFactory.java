abe609a642d828cf6b3be71ec6c02dcb7679deae
Jarylgapova <Karima.jarylgapova@gmail.com>
2020-04-18 18:38:17 -0500

    cucumberRunner

--- src/test/java/com/vytrack/utilities/DriverFactory.java
+++ src/test/java/com/vytrack/utilities/DriverFactory.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class DriverFactory {
     public static WebDriver createDriver(String browserName) {
         if (browserName.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().version("79.0").setup();
+            WebDriverManager.chromedriver().version("80").setup();
             return new ChromeDriver();
         } else {
             WebDriverManager.firefoxdriver().setup();
